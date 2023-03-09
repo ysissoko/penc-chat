5cf3b86ed6a4ae91f69e723ef45bedf742e1074d
Youssouf SISSOKO <ysissoko78@gmail.com>
2023-03-09 21:38:32 +0100

    feat: customer make offer feature

--- src/services/rest/schemas/message.schema.ts
+++ src/services/rest/schemas/message.schema.ts
@@ -4,4 +4,4 @@ import { ObjectSchema, TypeCode } from "pip-services3-commons-nodex";
 export default new ObjectSchema(false).withOptionalProperty('date', TypeCode.DateTime)
     .withRequiredProperty('senderUid', TypeCode.String)
     .withRequiredProperty('text', TypeCode.String)
-    .withOptionalProperty('offerId', TypeCode.String);
+    .withOptionalProperty('offerId', TypeCode.Integer);
