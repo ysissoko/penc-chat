f42485ee696e188f9b5c557deb0eedc5f6df62ec
ysissoko <ysissoko78@gmail.com>
2023-03-09 21:06:08 +0100

    feat: add product information to conversation (#6)

--- src/services/rest/schemas/conv.schema.ts
+++ src/services/rest/schemas/conv.schema.ts
@@ -1,6 +1,6 @@
 import { ObjectSchema, TypeCode } from "pip-services3-commons-nodex";
 
-
 export default new ObjectSchema(false)
     .withRequiredProperty('participantsUid', TypeCode.Array)
-    .withRequiredProperty('messages', TypeCode.Array);
+    .withRequiredProperty('messages', TypeCode.Array)
+    .withRequiredProperty('product', TypeCode.Map);
