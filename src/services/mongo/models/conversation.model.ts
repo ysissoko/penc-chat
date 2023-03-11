e368e67512284a4bbfd2e47c6dd1af8917996830
Youssouf SISSOKO <ysissoko78@gmail.com>
2023-03-11 15:23:45 +0100

    feat: add additionnal information in conversation (product and participants)

--- src/services/mongo/models/conversation.model.ts
+++ src/services/mongo/models/conversation.model.ts
@@ -1,5 +1,6 @@
 import { IIdentifiable } from 'pip-services3-commons-nodex';
 import { IMessage } from './message.model';
+import { IUser } from './user.model';
 
 /**
  * Data model identifiable representing a conversation
@@ -7,7 +8,16 @@ import { IMessage } from './message.model';
 export class Conversation implements IIdentifiable<string> {
     id!: string;
     date: Date = new Date;
-    participantsUid: string[] = [];
+    participants: IUser[] = [];
     messages: IMessage[] = [];
-    product!: { id: number, shopId: number };
+    product!: {
+        id: number,
+        category: { id: number, name: string },
+        price: number,
+        name: string,
+        description: string,
+        photos: string[],
+
+    };
+    shop!: { id: number, name: string }
 }
